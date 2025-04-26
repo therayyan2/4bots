cd7a37f1b5c652be3b41893d56a1c633070f1625
therayyan2 <therayyanahmd@gmail.com>
2025-04-26 12:37:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,35 +4,5 @@ st.title("⚡Explore Bots⚡")
 
 st.write("These bots have different features based on their roles. Explore now to check how these bots actually work.")
 
-algo, career, code, Edu, story = st.columns(5)
 
-with algo:
-    st.subheader("")
-    st.write("")
-    st.info("Orion Helps in understanding algorithms.")
-    st.page_link("pages/Algo-Smith.py", label="Explore Orion")
 
-with career:
-    st.subheader("")
-    st.write("")
-    st.info("Rezoom Helps to build resumes.")
-    st.page_link("pages/Career-Crafter.py", label="Explore Rezoom")
-
-with code:
-    st.subheader("")
-    st.write("")
-    st.info("Code-AI Helps in writing and fixing code.")
-    st.page_link("pages/Code-AI.py", label="Explore Code-AI")
-
-with Edu:
-    st.subheader("")
-    st.write("")
-    st.info("Study-Mentor Helps to solves problems.")
-    st.page_link("pages/Edu-Mate.py", label="Explore Studo")
-
-with story:
-    st.subheader("")
-    st.write("")
-    st.info("Forge Helps in Generating creative stories.")
-    st.page_link("pages/Story-Forge.py", label="Explore Forge")
-    
